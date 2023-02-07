cddb9f3b3b5e2559ae7e57a51b9a13a26f355070
Caique Giovannini <caique_giovannini@hotmail.com>
2023-02-06 22:21:32 -0300

    Config storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,5 +1,31 @@
-import { colors } from '@ignite-ui/tokens'
+import { ComponentProps } from '@stitches/react'
+import { styled } from './styles'
 
-export function App() {
-  return <h1 style={{ color: colors.ignite300 }}>Hello World</h1>
-}
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  borderRadius: '$sm',
+  padding: '$2 $4',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 12,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 14,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
