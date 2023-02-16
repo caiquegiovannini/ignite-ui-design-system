1ef8169dd2b3603d5ddb2bca61f382e24ca375d5
Caique Giovannini <caique_giovannini@hotmail.com>
2023-02-15 22:08:09 -0300

    Add Text stories

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi quae dolore sapiente recusandae, error libero atque odio reiciendis aut sed saepe molestiae placeat dignissimos suscipit dicta molestias fugiat sint ullam?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
